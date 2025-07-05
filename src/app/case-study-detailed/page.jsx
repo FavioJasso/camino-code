f9c96471086371cf41d7144b101426fa6e599a79
John <103416957+johnreycalesa@users.noreply.github.com>
2025-07-05 23:02:45 +0800

    FEAT: Refactor case study page to use ClientProviders
    Wrapped the CaseStudyDetailedPage component with ClientProviders and removed the Head import and usage. Also adjusted the layout and class names for improved structure.

--- src/app/case-study-detailed/page.jsx
+++ src/app/case-study-detailed/page.jsx
@@ -1,29 +1,21 @@
-import Head from 'next/head';
-import NavigationBar from '@/components/NavigationBar';
-import ContactForm from '@/components/ContactFormPages';
-import CaseStudyDetails from '@/components/CaseStudyDetails';
-import Footer from '@/components/Footer';
+"use client";
 
-const TITLE = "Case Study Detailed - My Next.js Site";
-const DESCRIPTION = "Explore our detailed case studies to see how we solve real-world problems.";
+import NavigationBar from "@/components/NavigationBar";
+import ContactForm from "@/components/ContactFormPages";
+import CaseStudyDetails from "@/components/CaseStudyDetails";
+import Footer from "@/components/Footer";
+import ClientProviders from "@/components/ClientProviders";
 
 const CaseStudyDetailedPage = () => {
   return (
-    <>
-      <Head>
-        <title>{TITLE}</title>
-        <meta name="description" content={DESCRIPTION} />
-      </Head>
-      
-      <main className="">
+    <ClientProviders>
+      <main className="h-max w-full gap-4 overflow-hidden">
         <NavigationBar />
-        <div className='bg-[rgba(248,244,239,1)]'>
-          <CaseStudyDetails />
-          <ContactForm />
-        </div>
+        <CaseStudyDetails />
+        <ContactForm />
         <Footer />
       </main>
-    </>
+    </ClientProviders>
   );
 };
 
