9fb2cd596dac08daeb05741954631c3f9510df53
John <103416957+johnreycalesa@users.noreply.github.com>
2025-07-05 23:03:20 +0800

    FEAT: Refactor ServicesPage layout and add ClientProviders
    Wrapped the ServicesPage component with ClientProviders and simplified the layout by removing background images and redundant divs. Updated component structure for improved maintainability and consistency.

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,48 +1,26 @@
-// app/services/page.tsx
+"use client";
+
 import NavigationBar from "@/components/NavigationBar";
 import ContactForm from "@/components/ContactFormPages";
 import ServicesHeader from "@/components/ServicesHeader";
 import ServicesList from "@/components/ServicesList";
 import ServicesDetails from "@/components/ServicesDetails";
 import Footer from "@/components/Footer";
-import Image from "next/image";
 import Work from "@/components/Work";
-
-export const metadata = {
-  title: "Our Services - Camino Code",
-  description: "Explore the range of services we offer to our clients.",
-};
+import ClientProviders from "@/components/ClientProviders";
 
 export default function ServicesPage() {
   return (
-    <main>
-      <NavigationBar iswhite={true} />
-      <ServicesHeader />
-      {/* Section with light background for services list */}
-      <div className="bg-[rgba(248,244,239,1)]">
+    <ClientProviders>
+      <main className="h-max w-full gap-4 overflow-hidden">
+        <NavigationBar />
+        <ServicesHeader />
         <ServicesList />
-      </div>
-      {/* Full-screen background image */}
-      <div className="relative h-screen w-full">
-        <Image
-          src="/assets/images/services_bg01.png"
-          alt="Services background"
-          fill
-          className="object-cover"
-          quality={90}
-          priority
-        />
-      </div>
-      <ServicesDetails />
-      {/* Section with light background for work and contact */}
-      <div className="bg-[rgba(248,244,239,1)]">
-        {/* Divider */}
-        <hr className="mx-auto w-full max-w-[1440px] border-gray-700" />
+        <ServicesDetails />
         <Work />
-        <hr className="mx-auto w-full max-w-[1440px] border-gray-700 mt-20" />
         <ContactForm />
-      </div>
-      <Footer />
-    </main>
+        <Footer />
+      </main>
+    </ClientProviders>
   );
 }
