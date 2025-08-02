354b9fee7bf190bffcf593ffb3e93c071c4dd518
John <103416957+johnreycalesa@users.noreply.github.com>
2025-08-02 10:46:46 +0800

    Refactor HeroSection layout and class order
    Reordered Tailwind CSS utility classes for consistency and readability in the HeroSection component. Adjusted layout properties to improve responsiveness and maintainability.

--- production/src/components/HeroSection.jsx
+++ production/src/components/HeroSection.jsx
@@ -4,35 +4,35 @@ import Link from "next/link";
 
 export default function HeroSection() {
   return (
-    <main className="relative w-full h-screen flex items-center justify-center mx-auto bg-[rgba(248,244,239,1)]">
+    <main className="relative mx-auto flex h-min w-full items-center justify-center bg-[rgba(248,244,239,1)]">
       {/* Top Right Spline */}
-      <div className="absolute md:top-32 top-0 -right-10 md:w-[315px] md:h-[318px] w-[250px] h-[250px]">
+      <div className="absolute top-0 -right-10 h-[250px] w-[250px] md:top-32 md:h-[318px] md:w-[315px]">
         <Image
           src="/assets/Vector-1.png"
           alt="Vector Graphic"
           width={500}
           height={500}
-          className="md:w-[315px] md:h-[318px] object-contain w-[230px] h-[230px]"
+          className="h-[230px] w-[230px] object-contain md:h-[318px] md:w-[315px]"
         />
       </div>
 
       {/* Bottom Left Spline */}
-      <div className="absolute md:-bottom-16 md:-left-32 -bottom-10 -left-10 md:w-[540px] md:h-[540px] w-[250px] h-[250px]">
+      <div className="absolute -bottom-10 -left-10 h-[250px] w-[250px] md:-bottom-16 md:-left-32 md:h-[540px] md:w-[540px]">
         <Image
           src="/assets/Vector.png"
           alt="Vector Graphic"
           width={500}
           height={500}
-          className="md:w-[468px] md:h-[468px] object-contain w-[230px] h-[230px]"
+          className="h-[230px] w-[230px] object-contain md:h-[468px] md:w-[468px]"
         />
       </div>
 
       {/* Centered Content */}
       <section
         id="home"
-        className="relative flex flex-col items-center justify-center gap-6 px-8 py-10 md:px-10 w-full text-center"
+        className="relative flex w-full flex-col items-center justify-center gap-6 px-8 py-10 text-center md:px-10"
       >
-        <h1 className="w-full font-bold uppercase text-6xl md:max-w-4xl md:text-6xl lg:text-6xl xl:text-[10rem] text-black">
+        <h1 className="w-full text-6xl font-bold text-black uppercase md:max-w-4xl md:text-6xl lg:text-6xl xl:text-[10rem]">
           <span className="inline-block bg-gradient-to-r from-amber-400 to-red-600 bg-clip-text text-transparent">
             Code
           </span>{" "}
@@ -44,10 +44,10 @@ export default function HeroSection() {
           custom web applications, we help businesses thrive in the digital age.
         </p>
 
-        <div className="flex gap-4 flex-col items-center">
+        <div className="flex flex-col items-center gap-4">
           <Link
             href="/contact"
-            className="rounded-full bg-gradient-to-t from-amber-400 to-red-600 px-6 py-3 text-white hover:opacity-90 flex items-center justify-center gap-1"
+            className="flex items-center justify-center gap-1 rounded-full bg-gradient-to-t from-amber-400 to-red-600 px-6 py-3 text-white hover:opacity-90"
           >
             Get Started
             <span className="ml-2">
