d7bb8a968a801883ee3009faa3cf8612a3c8c6da
John <103416957+johnreycalesa@users.noreply.github.com>
2025-08-02 10:45:59 +0800

    Refactor case study page for code style consistency
    Updated import statements and formatting in page.jsx to use double quotes and improve code readability. No functional changes were made.

--- production/src/app/case-study-detailed/page.jsx
+++ production/src/app/case-study-detailed/page.jsx
@@ -1,12 +1,13 @@
-import Head from 'next/head';
-import NavigationBar from '@/components/NavigationBar';
-import ContactForm from '@/components/ContactFormPages';
-import CaseStudyDetails from '@/components/CaseStudyDetails';
-import Footer from '@/components/Footer';
+import Head from "next/head";
+import NavigationBar from "@/components/NavigationBar";
+import ContactForm from "@/components/ContactFormPages";
+import CaseStudyDetails from "@/components/CaseStudyDetails";
+import Footer from "@/components/Footer";
 
 const CaseStudyDetailedPage = () => {
   const title = "Case Study Detailed - My Next.js Site";
-  const description = "Explore our detailed case studies to see how we solve real-world problems.";
+  const description =
+    "Explore our detailed case studies to see how we solve real-world problems.";
 
   return (
     <>
@@ -14,14 +15,14 @@ const CaseStudyDetailedPage = () => {
         <title>{title}</title>
         <meta name="description" content={description} />
       </Head>
-      
+
       <main className="">
         <NavigationBar />
-        <div  className='bg-[rgba(248,244,239,1)]'>
+        <div className="bg-[rgba(248,244,239,1)]">
           <CaseStudyDetails />
           <ContactForm />
         </div>
-        
+
         <Footer />
       </main>
     </>
