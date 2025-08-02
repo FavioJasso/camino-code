696a846e9fe0d95788d5047f0d59fa3029893274
John <103416957+johnreycalesa@users.noreply.github.com>
2025-08-02 10:46:07 +0800

    Refactor imports and JSX formatting in page.js
    Standardized import statements to use double quotes and updated JSX className attribute order for consistency. No functional changes were made.

--- production/src/app/page.js
+++ production/src/app/page.js
@@ -1,13 +1,13 @@
-import NavigationBar from '../components/NavigationBar';
-import HeroSection from '../components/HeroSection';
-import ContactForm from '../components/ContactFormPages';
-import Footer from '../components/Footer';
-import AboutUsPartners from '../components/AboutUsPartners';
-import HomeAbout from '../components/HomeAbout';
-import AboutUsOffers from '../components/AboutUsOffers';
-import HomeServices from '../components/HomeServices';
-import Work from '../components/Work';
-import Testimonials from '../components/Testimonials';
+import NavigationBar from "../components/NavigationBar";
+import HeroSection from "../components/HeroSection";
+import ContactForm from "../components/ContactFormPages";
+import Footer from "../components/Footer";
+import AboutUsPartners from "../components/AboutUsPartners";
+import HomeAbout from "../components/HomeAbout";
+import AboutUsOffers from "../components/AboutUsOffers";
+import HomeServices from "../components/HomeServices";
+import Work from "../components/Work";
+import Testimonials from "../components/Testimonials";
 
 const title = "Camino Code";
 const description = "This is my first page built with Astro!";
@@ -19,18 +19,18 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className="w-full h-max overflow-hidden gap-4">
+    <main className="h-max w-full gap-4 overflow-hidden">
       <NavigationBar />
       <HeroSection />
       <AboutUsPartners />
-      <div className='bg-[rgba(248,244,239,1)] p-2 md:p-4 lg:p-6 xl:p-8'>
+      <div className="bg-[rgba(248,244,239,1)] p-2 md:p-4 lg:p-6 xl:p-8">
         <HomeAbout />
       </div>
       <AboutUsOffers />
       <HomeServices />
       <Work />
       <Testimonials />
-      <div className='bg-[rgba(248,244,239,1)] p-2 md:p-4 lg:p-6 xl:p-8'>
+      <div className="bg-[rgba(248,244,239,1)] p-2 md:p-4 lg:p-6 xl:p-8">
         <ContactForm />
       </div>
       <Footer />
